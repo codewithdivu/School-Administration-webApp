e234e78a82025251045a3c7cf55118a1ebb28612
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-07-15 21:21:50 +0530

    first commit of this project

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Mavadiya Divyesh',
+  email: 'divu0017@gmail.com',
+  photoURL: '/static/mock-images/avatars/divu.jpg',
+};
+
+export default account;
