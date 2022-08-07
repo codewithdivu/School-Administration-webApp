651aaf4268e1e301cf0fe408acfeab9552f1f141
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-08-07 12:50:35 +0530

    fix firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: 'AIzaSyCt6YNeGxzBzJFo42aGWsUv1t6TOQVklIo',
+  authDomain: 'school-administration-c558d.firebaseapp.com',
+  projectId: 'school-administration-c558d',
+  storageBucket: 'school-administration-c558d.appspot.com',
+  messagingSenderId: '320945517532',
+  appId: '1:320945517532:web:c115a08b4fae197a0e9519',
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 
