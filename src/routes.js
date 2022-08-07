de48219b19cbeb3d674365d2bb0bc1f101b9229d
codewithdivu <mavadiyadivyesh56@gmail.com>
2022-08-07 12:40:42 +0530

    removed warning

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,6 @@ import Blog from './pages/Blog';
 import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
-import Register from './pages/Register';
 import Products from './pages/Products';
 import DashboardApp from './pages/DashboardApp';
 import useAuthenticateUser from './hooks/useAuthenticateUser';
